4f11ea7e6f77961c13c6607f9f4fbcdeb9a238fb
MohamedSalmankhan <45882892+MohamedSalmankhan@users.noreply.github.com>
2019-01-23 20:17:46 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,8 +2,11 @@ variable "instance-ami" {
   default = "ami-0080e4c5bc078760e"
 }
 variable "vpc-id" {
+  default = "vpc-6052e104"
 }
 variable "subnet-id" {
+  default = "subnet-0cbc1c31"
 }
 variable "admin-cidr" {
+  default = "42.109.129.232/32"
 }
